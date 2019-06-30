a897f4b922ca8de733ed2aa709c6e1706f9c9af2
yt <yt13550985419@126.com>
2019-06-30 20:08:09 +0800

    idea修改

--- test/src/com/itheima/Test.java
+++ test/src/com/itheima/Test.java
@@ -7,7 +7,9 @@ public class Test {
       
 		System.out.println("如今的现在早已不是曾经说好的未来");
         System.out.println("加油,少年");
-        System.out.println("加油,少年111");
+        System.out.println("加油,少年");
+        System.out.println("加油,少年");
+
 
 
 
