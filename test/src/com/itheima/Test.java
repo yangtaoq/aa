9686012077562195759b138a7a38da0f23f553b7
yt <yt13550985419@126.com>
2019-06-30 19:06:16 +0800

    修改

--- test/src/com/itheima/Test.java
+++ test/src/com/itheima/Test.java
@@ -4,15 +4,8 @@ package com.itheima;
 public class Test {
     public static void main(String[] args) {
         System.out.println("helloworld");
-		System.out.println("helloworldccc999");
-		System.out.println("helloworldccc123");
-
-		//如今的现在早已不是曾经说好的未来
-//haha122222
-//你好明天1
-
-
-
+        System.out.println("主线");
+		
 
     }
 
